d315be0adba9cef4bb5cbc537c400af151942f8c
lumilio <logistic-division@lumilium.com>
2021-11-12 03:39:54 +0100

    svolgimento ultima parte footer

--- src/components/Footermenu.vue
+++ src/components/Footermenu.vue
@@ -1,32 +1,79 @@
 <template>
-  <div class="hello">
-      xxx
-  </div>
+    <div class="box-C5">
+        <div class="layout-box">
+            <div class="centered-box">
+                <button>xxxx</button>
+                <div>
+                    <a href="#">FOLLOW US</a>
+                    <ul id='header-list'>
+                        <li v-for="link in links" v-bind:key="link.id"> <a href="#"><img :src="require(`../assets/img/${link}`)" alt=""></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'C5',
+  components:{},
+    data() {
+        return {
+            links: [
+                'footer-facebook.png',
+                'footer-twitter.png',
+                'footer-youtube.png',
+                'footer-pinterest.png',
+                'footer-periscope.png',
+            ]
+        };
+    },
   props: {
     msg: String
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+@import '../assets/scss/variables.scss';
+.box-C1{
+    @include layout-block-1;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.layout-box{
+    @include layout-block-1;
+    background-color: #303030;
+    z-index: 1;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.centered-box{
+    @include layout-block-2;
+    justify-content: space-between;
 }
-a {
-  color: #42b983;
+#header-logo{
+    width: 67px;
+    height: 67px;
+}
+#header-list{
+    margin: 0;
+}
+#header-list li{
+    display: inline-block;
+    margin: 0 14px;
+}
+#header-list a{
+    text-decoration: none;
+    font-size: 11px;
+    line-height: 85px;
+    color: $sie-color-1;
+}
+#header-list li:hover{
+    border-bottom: 5px solid $sie-color-2;
+}
+#header-list li:hover > a{
+    color: $sie-color-2;
 }
 </style>
--- src/components/Listbox.vue
+++ src/components/Listbox.vue
@@ -2,52 +2,51 @@
     <div class="box-C4">
         <div class="layout-box">
             <div class="centered-box">
-                <div>
+                <div class="mini-list">
                     <div>
                         <h3>DC COMICS</h3>
                         <ul>
-                            <li>ss</li>
-                            <li>ssss</li>
-                            <li>ssss</li>
-                            <li>ss</li>
-                            <li>sss</li>
-                            <li>sss</li>
-                            <li>ss</li>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
                         </ul>
                     </div>
                     <div>
                         <h3>SHOP</h3>
                         <ul>
-                            <li>ss</li>
-                            <li>ssss</li>
-                            <li>ssss</li>
-                            <li>ss</li>
-                            <li>sss</li>
-                            <li>sss</li>
-                            <li>ss</li>
+                            <li><a href="#">Shop DC</a> </li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
                         </ul>
                     </div>
                 </div>
-                <div>
+                <div class="mini-list">
                     <h3>DC</h3>
                     <ul>
-                        <li>ss</li>
-                        <li>ss</li>
-                        <li>ss</li>
-                        <li>ss</li>
-                        <li>ss</li>
-                        <li>ss</li>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy policy (New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshop</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
                     </ul>
                 </div>
-                <div>
+                <div class="mini-list">
                     <h3>SITES</h3>
                     <ul>
-                        <li>ss</li>
-                        <li>ss</li>
-                        <li>ss</li>
-                        <li>ss</li>
-                        <li>ss</li>
-                        <li>ss</li>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
                     </ul>
                 </div>
                 <img id='footer-logo' src="../assets/img/dc-logo-bg.png" alt="">
@@ -60,7 +59,7 @@
 
 <script>
 export default {
-  name: 'C1',
+  name: 'C4',
   components:{},
     data() {
         return {
@@ -83,10 +82,6 @@ export default {
 
 <style scoped lang="scss">
 @import '../assets/scss/variables.scss';
-.box-C1{
-    @include layout-block-1;
-
-}
 .layout-box{
     @include layout-block-1;
     background-image: url('../assets/img/footer-bg.jpg') ;
@@ -100,15 +95,32 @@ export default {
     width: 1140px;
     display: flex;
     position: relative;
-    height: 500px;
     color: white;
+}
+.mini-list{
+    margin: 30px 70px 40px 0px;
+}
+.centered-box h3{
+    margin-bottom: 0;
+}
+.centered-box ul{
+    padding: 0;
+    margin-top: 10px;
+    list-style: none;
+}
+.centered-box li a{
+    color: #636b6b;
+    text-decoration: none;
+    font-size: smaller;
+}
+.centered-box li a:hover{
+    color: #abb5b5;
+    text-decoration: underline;
 }
 .centered-box img{
     position: absolute;
     right: 0;
     top: -95px;
-
 }
-
 </style>
 
